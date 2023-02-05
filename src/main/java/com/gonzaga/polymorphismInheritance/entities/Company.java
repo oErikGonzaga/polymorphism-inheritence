bfc15e616f12421ad7d359c4d6b4fb4a10f6df59
erikgonzaga <oerikgonzaga@outlook.com>
2023-02-05 18:12:53 -0300

    Implementacao da logica de calculo de taxa

--- src/main/java/com/gonzaga/polymorphismInheritance/entities/Company.java
+++ src/main/java/com/gonzaga/polymorphismInheritance/entities/Company.java
@@ -1,4 +1,23 @@
 package com.gonzaga.polymorphismInheritance.entities;
 
-public class Company {
+public class Company extends TaxPayer{
+
+    private int numberOfEmployees;
+
+    public Company(String name, double annualIncome, int numberOfEmployees) {
+        super(name, annualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public double tax() {
+        return getNumberOfEmployees() > 10 ? getAnnualIncome() * 0.14 : getAnnualIncome() * 0.16;
+    }
 }
--- src/main/java/com/gonzaga/polymorphismInheritance/entities/Individual.java
+++ src/main/java/com/gonzaga/polymorphismInheritance/entities/Individual.java
@@ -1,4 +1,32 @@
 package com.gonzaga.polymorphismInheritance.entities;
 
-public class Individual {
+public class Individual extends TaxPayer{
+
+    private double healthExpenditures;
+
+    public Individual(String name, double annualIncome, double healthExpenditures) {
+        super(name, annualIncome);
+        this.healthExpenditures = healthExpenditures;
+    }
+    public double getHealthExpenditures() {
+        return healthExpenditures;
+    }
+    public void setHealthExpenditures(double healthExpenditures) {
+        this.healthExpenditures = healthExpenditures;
+    }
+
+    @Override
+    public double tax() {
+
+        double basicTax;
+
+        basicTax = getAnnualIncome() < 20000 ? getAnnualIncome() * 0.15 : getAnnualIncome() * 0.25;
+        basicTax -= getHealthExpenditures() * 0.5;
+
+        if (basicTax < 0)
+            basicTax = 0;
+
+        return basicTax;
+    }
+
 }
