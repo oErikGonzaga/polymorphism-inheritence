8b8697a982e41bfc24602fea3b076427e48032a3
erikgonzaga <oerikgonzaga@outlook.com>
2023-02-04 22:24:40 -0300

    Iniciando

---
+++ src/main/java/com/gonzaga/polymorphismInheritance/PolymorphismInheritanceApplication.java
@@ -0,0 +1,13 @@
+package com.gonzaga.polymorphismInheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolymorphismInheritanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolymorphismInheritanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonzaga/polymorphismInheritance/PolymorphismInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzaga.polymorphismInheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolymorphismInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
